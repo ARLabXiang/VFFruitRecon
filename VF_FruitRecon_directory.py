9f568765aa536593f2c1a7ebf4d73fcee3fe7c06
XingjianL <lixingjian2002@gmail.com>
2026-02-19 16:48:54 -0500

    main script and sample images

---
+++ VF_FruitRecon_directory.py
@@ -0,0 +1,244 @@
+import sys
+import os
+PATH = os.getcwd()
+sys.path.append(f"{PATH}/sam-3d-objects/notebook")
+sys.path.append(f"{PATH}/Grounded-Segment-Anything")
+import imageio
+import numpy as np
+import torch
+from inference import Inference, ready_gaussian_for_video_rendering, load_masks, make_scene, render_video
+import imageio.v3 as iio
+import gc
+import psutil
+from loguru import logger
+from tqdm import tqdm
+from helper import transform_mesh_vertices, load_image, get_grounding_output, save_mask_data, load_gsam_model, set_seed, filter_larger_covering_masks
+from segment_anything import (
+    sam_model_registry,
+    sam_hq_model_registry,
+    SamPredictor
+)
+
+from omegaconf import OmegaConf
+from FoundationStereo.core.foundation_stereo import FoundationStereo
+from FoundationStereo.core.utils.utils import InputPadder
+import cv2
+
+def clear_cache():
+    torch.cuda.empty_cache()
+    print(torch.cuda.memory_allocated() / 1e9,
+      torch.cuda.memory_reserved() / 1e9)
+    gc.collect()
+    process = psutil.Process(os.getpid())
+    print("RAM (GB):", process.memory_info().rss / 1e9)
+
+run_name = "output"
+leftimages_dir = "sample_image/left"
+rightimages_dir = "sample_image/right"
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+### GroundedSAM Configs
+text_prompt = "tomato"
+gsam_config = "Grounded-Segment-Anything/GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py"
+grounded_checkpoint = "model_checkpoints/GSAM/groundingdino_swint_ogc.pth"
+sam_checkpoint = "model_checkpoints/GSAM/sam_vit_h_4b8939.pth"
+sam_hq = False
+sam_version = "vit_h"
+### FS Configs
+fs_checkpoint = "model_checkpoints/FS/model_best_bp2.pth"
+### SAM-3D Configs
+#exit()
+TAG = "hf"
+sam3d_config_path = f"model_checkpoints/SAM3D/pipeline.yaml"
+sam3d_data_directory = f"{run_name}/sam3d_inputs"
+sam3d_output_folder = f"{run_name}/sam3d_outputs"
+Baseline = 39.184552621463276 / 1000.
+focallength = 1729.537316070467
+K = np.array([
+    [focallength, 0.0,      1211.229625426932],
+    [0.0,        focallength, 1018.251638296653],
+    [0.0,        0.0,        1.0]
+], dtype=np.float32)
+
+#### Begin VF-FruitRecon
+grounded_sam_model = load_gsam_model(gsam_config, grounded_checkpoint, None, device=device)
+if sam_hq:
+    sam_predictor = SamPredictor(sam_hq_model_registry[sam_version](checkpoint=sam_checkpoint).to(device))
+else:
+    sam_predictor = SamPredictor(sam_model_registry[sam_version](checkpoint=sam_checkpoint).to(device))
+for left_img in os.listdir(leftimages_dir):
+    rgb_img, dino_img = load_image(f"{leftimages_dir}/{left_img}")
+    boxes_filt, pred_phrases = get_grounding_output(
+        grounded_sam_model, dino_img, text_prompt, 0.3, 0.25, device=device
+    )
+    sam_predictor.set_image(rgb_img)
+    size = rgb_img.shape
+    print(rgb_img.shape)
+    H, W = size[0], size[1]
+    for i in range(boxes_filt.size(0)):
+        boxes_filt[i] = boxes_filt[i] * torch.Tensor([W, H, W, H])
+        boxes_filt[i][:2] -= boxes_filt[i][2:] / 2
+        boxes_filt[i][2:] += boxes_filt[i][:2]
+
+    boxes_filt = boxes_filt.cpu()
+    transformed_boxes = sam_predictor.transform.apply_boxes_torch(boxes_filt, rgb_img.shape[:2]).to(device)
+
+    masks, _, _ = sam_predictor.predict_torch(
+        point_coords = None,
+        point_labels = None,
+        boxes = transformed_boxes.to(device),
+        multimask_output = False,
+    )
+    
+    save_mask_data(sam3d_data_directory, masks, boxes_filt, pred_phrases,outname=os.path.basename(left_img)[:-4])
+    mask_files = sorted(
+        [f"{sam3d_data_directory}/{os.path.basename(left_img)[:-4]}/{f}" 
+         for f in os.listdir(f"{sam3d_data_directory}/{os.path.basename(left_img)[:-4]}")
+         if "png" in f])
+    masks = [np.where(cv2.imread(f)[:,:,0] > 0, 1, 0) for f in mask_files]
+    keep_indices, children = filter_larger_covering_masks(masks)
+    filtered_masks = [masks[i] for i in keep_indices]
+    files_to_delete = []
+    for i in keep_indices:
+        if len(children[i]) > 1: # delete the lar
+            files_to_delete.append(mask_files[i])
+        else: #delete the children
+            files_to_delete.extend([mask_files[j] for j in children[i]])
+    for f in files_to_delete:
+        if os.path.exists(f):
+            os.remove(f)
+clear_cache()
+del grounded_sam_model, sam_predictor
+clear_cache()
+## Begin of Foundation Stereo
+fs_cfg = OmegaConf.load(f'{os.path.dirname(fs_checkpoint)}/cfg.yaml')
+#fs_cfg['vit_size'] = 'vitl'
+fs_cfg["valid_iters"] = 32
+fs_cfg["hiera"] = 1
+fs_cfg["z_far"] = 10
+fs_cfg["scale"] = 1
+set_seed(0)
+torch.autograd.set_grad_enabled(False)
+fs_args = OmegaConf.create(fs_cfg)
+fs_model = FoundationStereo(fs_args)
+fs_checkpoint = torch.load(fs_checkpoint)
+fs_model.load_state_dict(fs_checkpoint['model'])
+fs_model.cuda()
+fs_model.eval()
+
+for left_img, right_img in zip(sorted(os.listdir(leftimages_dir)), 
+                               sorted(os.listdir(rightimages_dir))):
+    print(left_img, right_img)
+    img0 = imageio.imread(f"{leftimages_dir}/{left_img}")
+    img1 = imageio.imread(f"{rightimages_dir}/{right_img}")
+    
+    img0 = cv2.resize(img0, fx=1, fy=1, dsize=None)
+    img1 = cv2.resize(img1, fx=1, fy=1, dsize=None)
+    H,W = img0.shape[:2]
+    img0_ori = img0.copy()
+    img0 = torch.as_tensor(img0).cuda().float()[None].permute(0,3,1,2)
+    img1 = torch.as_tensor(img1).cuda().float()[None].permute(0,3,1,2)
+    padder = InputPadder(img0.shape, divis_by=32, force_square=False)
+    img0, img1 = padder.pad(img0, img1)
+    with torch.autocast("cuda"):
+        disp = fs_model.run_hierachical(img0, img1, iters=fs_args.valid_iters, test_mode=True, small_ratio=0.5)
+    disp = padder.unpad(disp.float())
+    disp = disp.data.cpu().numpy().reshape(H,W)
+    cv2.imwrite(f"{sam3d_data_directory}/{os.path.basename(left_img)[:-4]}/disp.tiff", disp)
+clear_cache()
+del fs_model
+clear_cache()
+## Begin of SAM-3D
+inference = Inference(sam3d_config_path, compile=False)
+directories = sorted(os.listdir(sam3d_data_directory))
+logger.remove()
+logger.add(
+    sys.stderr,
+    filter=lambda record: record["level"].name != "INFO"
+)
+
+for left_img_path in os.listdir(leftimages_dir):
+    filename_dir = os.path.basename(left_img_path)[:-4]
+    print(filename_dir)
+    left_img, _ = load_image(f"{leftimages_dir}/{left_img_path}")
+    sam3d_input_dir = f"{sam3d_data_directory}/{filename_dir}"
+    print(sam3d_input_dir)
+    disp_img_path = f"{sam3d_input_dir}/disp.tiff"
+    mask_list = sorted([
+        int(os.path.splitext(f)[0])
+        for f in os.listdir(sam3d_input_dir)
+        if f.endswith(".png")
+    ])
+    print(mask_list)
+    masks = load_masks(sam3d_input_dir, indices_list= mask_list, extension=".png")
+    if len(masks) == 0:
+        continue
+    disparity = iio.imread(disp_img_path).astype(np.float32)
+    disparity[disparity <= 0] = np.nan
+    depth = focallength*Baseline/disparity
+    depth[depth > 5] = np.nan
+    depth[depth <= 0] = np.nan  
+    
+    H, W = depth.shape
+    
+    fx = K[0, 0]
+    fy = K[1, 1]
+    cx = K[0, 2]
+    cy = K[1, 2]
+    
+    u = np.arange(W)
+    v = np.arange(H)
+    uu, vv = np.meshgrid(u, v)
+    
+    Z = depth
+    X = (uu - cx) * Z / fx
+    Y = (vv - cy) * Z / fy
+    valid_mask = np.isfinite(Z)
+    # Convert to right-handed PyTorch3D coordinates
+    pointmap = np.stack([-X, -Y, Z], axis=-1)
+    pointmaP = torch.tensor(pointmap, dtype=torch.float32)
+
+    print("Reconstructing tomato meshes ... may take a long time")
+    outputs = [
+        inference(left_img, mask, seed=42, pointmap=pointmaP)
+        for mask in tqdm(masks, desc="Running inference")
+    ]
+
+    os.makedirs(f"{sam3d_output_folder}/{filename_dir}", exist_ok=True)
+    for i, out in enumerate(outputs):
+        mesh = out["glb"]
+        vertices = mesh.vertices
+        vertices_tensor = torch.tensor(vertices)
+    
+        S = out["scale"][0].cpu().float()
+        T = out["translation"][0].cpu().float()
+        R = out["rotation"].squeeze().cpu().float()
+    
+        vertices_transformed = transform_mesh_vertices(vertices, R, T, S)
+        mesh.vertices = vertices_transformed.cpu().numpy().astype(np.float32)
+        #mesh = mesh.simplify_quadric_decimation(face_count=2000)
+        mesh.export(f"{sam3d_output_folder}/{filename_dir}/mesh_{mask_list[i]}.ply")
+    
+    scene_gs = make_scene(*outputs)
+    scene_gs = ready_gaussian_for_video_rendering(scene_gs)
+    video = render_video(
+        scene_gs,
+        r=1,
+        fov=60,
+        resolution=1024,
+    )["color"]
+    # save video as gif
+    imageio.mimsave(
+        os.path.join(f"{sam3d_output_folder}/{filename_dir}/render.gif"),
+        video,
+        format="GIF",
+        duration=1000 / 30,  # default assuming 30fps from the input MP4
+        loop=0,  # 0 means loop indefinitely
+    )
+    # clear cache
+    clear_cache()
+    del outputs, scene_gs, video
+    clear_cache()
+
+print()
+print("All completed")
---
+++ helper.py
@@ -0,0 +1,154 @@
+import sys
+import os
+sys.path.append(os.path.join(os.getcwd(), "Grounded-Segment-Anything/GroundingDINO"))
+import numpy as np
+import torch
+from pytorch3d.transforms import quaternion_to_matrix, Transform3d
+from PIL import Image
+import groundingdino.datasets.transforms as T
+from groundingdino.util.utils import clean_state_dict, get_phrases_from_posmap
+from groundingdino.util.slconfig import SLConfig
+from groundingdino.models import build_model
+
+import cv2
+import matplotlib.pyplot as plt
+import json
+def load_image(path):
+    image = Image.open(path)
+    transform = T.Compose(
+        [
+            T.RandomResize([800], max_size=1333),
+            T.ToTensor(),
+            T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    )
+    image_dino, _ = transform(image, None)
+    image = np.array(image)
+    image = image.astype(np.uint8)
+    return image, image_dino
+def get_grounding_output(model, image, caption, box_threshold, text_threshold, with_logits=True, device="cpu"):
+    caption = caption.lower()
+    caption = caption.strip()
+    if not caption.endswith("."):
+        caption = caption + "."
+    model = model.to(device)
+    image = image.to(device)
+    with torch.no_grad():
+        outputs = model(image[None], captions=[caption])
+    logits = outputs["pred_logits"].cpu().sigmoid()[0]  # (nq, 256)
+    boxes = outputs["pred_boxes"].cpu()[0]  # (nq, 4)
+    logits.shape[0]
+
+    # filter output
+    logits_filt = logits.clone()
+    boxes_filt = boxes.clone()
+    filt_mask = logits_filt.max(dim=1)[0] > box_threshold
+    logits_filt = logits_filt[filt_mask]  # num_filt, 256
+    boxes_filt = boxes_filt[filt_mask]  # num_filt, 4
+    logits_filt.shape[0]
+
+    # get phrase
+    tokenlizer = model.tokenizer
+    tokenized = tokenlizer(caption)
+    # build pred
+    pred_phrases = []
+    for logit, box in zip(logits_filt, boxes_filt):
+        pred_phrase = get_phrases_from_posmap(logit > text_threshold, tokenized, tokenlizer)
+        if with_logits:
+            pred_phrases.append(pred_phrase + f"({str(logit.max().item())[:4]})")
+        else:
+            pred_phrases.append(pred_phrase)
+
+    return boxes_filt, pred_phrases
+def transform_mesh_vertices(vertices, rotation, translation, scale):
+    R_yup_to_zup = torch.tensor([[-1,0,0],[0,0,1],[0,1,0]], dtype=torch.float32)
+    R_flip_z = torch.tensor([[1,0,0],[0,1,0],[0,0,-1]], dtype=torch.float32)
+    R_pytorch3d_to_cam = torch.tensor([[-1,0,0],[0,-1,0],[0,0,1]], dtype=torch.float32)
+
+    if isinstance(vertices, np.ndarray):
+        vertices = torch.tensor(vertices, dtype=torch.float32)
+
+    vertices = vertices.unsqueeze(0)  #  batch dimension [1, N, 3]
+    vertices = vertices @ R_flip_z.to(vertices.device) 
+    vertices = vertices @ R_yup_to_zup.to(vertices.device)
+    R_mat = quaternion_to_matrix(rotation.to(vertices.device))
+    tfm = Transform3d(dtype=vertices.dtype, device=vertices.device)
+    tfm = (
+        tfm.scale(scale)
+           .rotate(R_mat)
+           .translate(translation[0], translation[1], translation[2])
+    )
+    vertices_world = tfm.transform_points(vertices)
+    vertices_world = vertices_world @ R_pytorch3d_to_cam.to(vertices_world.device)
+    
+    return vertices_world[0]  # remove batch dimension
+def save_mask_data(output_dir, mask_list, box_list, label_list, outname="mask"):
+    value = 0  # 0 for background
+
+    mask_img = torch.zeros(mask_list.shape[-2:])
+    os.makedirs(os.path.join(output_dir, f'{outname}'), exist_ok=True)
+    for idx, mask in enumerate(mask_list):
+        current_mask = torch.zeros(mask_list.shape[-2:])
+
+        mask_img[mask.cpu().numpy()[0] == True] = value + idx + 1
+        current_mask[mask.cpu().numpy()[0] == True] = 255
+        cv2.imwrite(os.path.join(output_dir, f'{outname}/{idx+1}.png'), current_mask.numpy())
+    plt.figure(figsize=(10, 10))
+    plt.imshow(mask_img.numpy())
+    plt.axis('off')
+    plt.savefig(os.path.join(output_dir, f'{outname}/vis.jpg'), bbox_inches="tight", dpi=300, pad_inches=0.0)
+
+    json_data = [{
+        'value': value,
+        'label': 'background'
+    }]
+    for label, box in zip(label_list, box_list):
+        value += 1
+        name, logit = label.split('(')
+        logit = logit[:-1] # the last is ')'
+        json_data.append({
+            'value': value,
+            'label': name,
+            'logit': float(logit),
+            'box': box.numpy().tolist(),
+        })
+    with open(os.path.join(output_dir, f'{outname}.json'), 'w') as f:
+        json.dump(json_data, f)
+def load_gsam_model(model_config_path, model_checkpoint_path, bert_base_uncased_path, device):
+    args = SLConfig.fromfile(model_config_path)
+    args.device = device
+    args.bert_base_uncased_path = bert_base_uncased_path
+    model = build_model(args)
+    checkpoint = torch.load(model_checkpoint_path, map_location="cpu", weights_only=True)
+    load_res = model.load_state_dict(clean_state_dict(checkpoint["model"]), strict=False)
+    print(load_res)
+    _ = model.eval()
+    return model
+def filter_larger_covering_masks(masks, containment_thresh=0.9, min_children=1):
+    n = len(masks)
+    areas = np.array([np.count_nonzero(m) for m in masks])
+
+    order = np.argsort(-areas)
+    children = {i: [] for i in range(n)}
+
+    for idx_i, i in enumerate(order):
+        for j in order[idx_i + 1:]:
+            if areas[j] >= areas[i]:
+                continue
+
+            inter = np.logical_and(masks[i], masks[j]).sum()
+            if inter / (areas[j] + 1e-8) >= containment_thresh:
+                children[i].append(j)
+
+    # keep only masks that actually cover others
+    keep = [i for i, ch in children.items() if len(ch) >= min_children]
+
+    return keep, children
+def set_seed(random_seed):
+  import torch,random
+  np.random.seed(random_seed)
+  random.seed(random_seed)
+  torch.manual_seed(random_seed)
+  torch.cuda.manual_seed_all(random_seed)
+  torch.backends.cudnn.deterministic = True
+  torch.backends.cudnn.benchmark = False
